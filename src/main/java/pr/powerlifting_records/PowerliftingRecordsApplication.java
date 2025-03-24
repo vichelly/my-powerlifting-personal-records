ec8cdb36a16b5235a749379bfcfec181166c36cc
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-03-24 20:13:21 -0300

    feat: first commit

---
+++ src/main/java/pr/powerlifting_records/PowerliftingRecordsApplication.java
@@ -0,0 +1,13 @@
+package pr.powerlifting_records;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerliftingRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerliftingRecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pr/powerlifting_records/PowerliftingRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package pr.powerlifting_records;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerliftingRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
