5a99e8281f456cf99c25ca0ae28ca5f0f3549bbf
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-03-25 17:23:05 -0300

    feat: usei lombok

--- src/main/java/pr/powerlifting_records/Model/LifterModel.java
+++ src/main/java/pr/powerlifting_records/Model/LifterModel.java
@@ -2,17 +2,22 @@ package pr.powerlifting_records.Model;
 
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@EntityScan
+@Entity
 @Table(name = "tb_lifter")
+@NoArgsConstructor // lombok cria construtor sem parametros automaticamente
+@AllArgsConstructor // lombok cria contrutor com todos os parametros autmaticamente
+@Data // loombo cria todos os getters e setters criados automaticamente 
 public class LifterModel {
 
     @Id
@@ -21,41 +26,7 @@ public class LifterModel {
     private String name;
     private String password;
     
-    // um lifter tem vários pr
+    // um lifter tem vários pr, mapear os pr por lifter
     @OneToMany(mappedBy = "lifter", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PrModel> pr;
-
-    public LifterModel(){}
-    
-    public LifterModel(String name, String password, List<PrModel> pr){
-        this.name = name;
-        this.password = password;
-        this.pr = pr;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public List<PrModel> getPr() {
-        return pr;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public void setPr(List<PrModel> pr) {
-        this.pr = pr;
-    }
 }
--- src/main/java/pr/powerlifting_records/Model/PrModel.java
+++ src/main/java/pr/powerlifting_records/Model/PrModel.java
@@ -1,53 +1,32 @@
 package pr.powerlifting_records.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_pr")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PrModel {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id; 
 
     @Enumerated(EnumType.STRING)
-    private exercise exercise;
+    private Exercise exercise;
+
     private float kg;
 
-    // 1 pr tem um lifter
+    // 1 PR tem um lifter
     @ManyToOne
-    @JoinColumn(name = "lifter_id", nullable = false)
+    @JoinColumn(name = "lifter_id", nullable = false) // lifter_id = foreign key
     private LifterModel lifter;
 
-    public enum exercise {
-        Squad, Benchpress, Deadlift
-    }
-
-    public PrModel(){}
-
-    public PrModel(exercise exercise, float kg){
-        this.exercise = exercise;
-        this.kg = kg;
-    }
-
-    public exercise getExercise() {
-        return exercise;
-    }
-    public float getKg() {
-        return kg;
-    }
-
-    public void setExercise(exercise exercise) {
-        this.exercise = exercise;
-    }
-    public void setKg(float kg) {
-        this.kg = kg;
+    public enum Exercise {
+        SQUAT, BENCHPRESS, DEADLIFT
     }
 }
