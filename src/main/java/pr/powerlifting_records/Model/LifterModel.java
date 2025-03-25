0361cbfd852ad6267e2adde393e12d1a7893c862
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-03-24 22:57:42 -0300

    feat: arrumei o pom, para que consiga usar o mysql

--- src/main/java/pr/powerlifting_records/Model/LifterModel.java
+++ src/main/java/pr/powerlifting_records/Model/LifterModel.java
@@ -1,12 +1,32 @@
 package pr.powerlifting_records.Model;
 
-public class LifterModel {
-    String name;
-    String password;
-    Long id;
-    PrModel pr;
+import java.util.List;
 
-    public LifterModel(String name, String password, PrModel pr){
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@EntityScan
+@Table(name = "lifter")
+public class LifterModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String password;
+    
+    @OneToMany(mappedBy = "lifter", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PrModel> pr;
+
+    public LifterModel(){}
+    
+    public LifterModel(String name, String password, List<PrModel> pr){
         this.name = name;
         this.password = password;
         this.pr = pr;
@@ -21,7 +41,7 @@ public class LifterModel {
     public String getPassword() {
         return password;
     }
-    public PrModel getPr() {
+    public List<PrModel> getPr() {
         return pr;
     }
 
@@ -34,7 +54,7 @@ public class LifterModel {
     public void setPassword(String password) {
         this.password = password;
     }
-    public void setPr(PrModel pr) {
+    public void setPr(List<PrModel> pr) {
         this.pr = pr;
     }
 }
--- src/main/java/pr/powerlifting_records/Model/PrModel.java
+++ src/main/java/pr/powerlifting_records/Model/PrModel.java
@@ -1,9 +1,21 @@
 package pr.powerlifting_records.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class PrModel {
-    enum exercise {Squad, Benchpress, Deadlift};
-    exercise exercise;
-    float kg;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private enum exercise {Squad, Benchpress, Deadlift};
+    private exercise exercise;
+    private float kg;
+
+    public PrModel(){}
 
     public PrModel(exercise exercise, float kg){
         this.exercise = exercise;
