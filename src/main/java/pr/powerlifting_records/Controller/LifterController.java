14ab520f4188a752d5c1f412114c7a13ab54baff
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-04-02 14:47:14 -0300

    fix: deleted an not using import

--- src/main/java/pr/powerlifting_records/Controller/LifterController.java
+++ src/main/java/pr/powerlifting_records/Controller/LifterController.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
