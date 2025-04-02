d7cd982ebe53a46ee33bdcbbd6471b7821cb4019
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-04-02 14:45:51 -0300

    feat: created the neww endpoint wich allows me to pick one lifter by his ID

--- src/main/java/pr/powerlifting_records/Controller/LifterController.java
+++ src/main/java/pr/powerlifting_records/Controller/LifterController.java
@@ -12,6 +12,9 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -32,6 +35,14 @@ public class LifterController {
         return lifterRepository.findAll();
     }
     
+    @GetMapping("/{id}")
+    public LifterModel getLifterById(@PathVariable Long id) {
+        Optional<LifterModel> lifterOptional = lifterRepository.findById(id);
+        LifterModel lifter = lifterOptional.get();
+        return lifter;
+    }
+    
+
     @PostMapping
     public LifterModel createLifter(@RequestBody LifterModel lifter) {
         return lifterRepository.save(lifter);
